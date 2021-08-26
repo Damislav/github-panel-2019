73ac62fba58fcd61d1eaf1983d3e1f9db9855cb7
Damislav <damislav@net.hr>
2021-08-26 22:32:31 +0200

    #

--- src/App.js
+++ src/App.js
@@ -82,6 +82,17 @@ class App extends React.Component {
       users: [],
       loading: false,
     });
+    setTimeout(async () => {
+      this.setState({ loading: true });
+      const res = await axios.get(
+        `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      );
+
+      this.setState({
+        users: res.data,
+        loading: false,
+      });
+    }, 2500);
   };
 
   // şet alert
